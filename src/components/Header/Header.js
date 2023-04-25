2e62e1643e5cef2db46c6a3ae936ff7dd2154849
Nima Bargestan <nimam700@gmail.com>
2023-04-25 17:44:45 -0400

    create templete for header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import LogoImg from "../../assests/Logo/InStock-Logo.svg";
+import "./Header.scss";
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo-wrapper">
+          <img src={LogoImg} alt="inStock Logo" />
+        </div>
+        <nav className="nav">
+          <ul>
+            <a className="nav__item" href="/">
+              Warehouses
+            </a>
+            <a className="nav__item" href="/">
+              Inventory
+            </a>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
