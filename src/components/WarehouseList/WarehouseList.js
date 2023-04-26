650d4709cdc9c86d7a8269f289a870ad7e10bae0
James Zuliani <zulianijames@gmail.com>
2023-04-26 17:31:41 -0400

    tablet styling initialized for warehouse list

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
-import vectorIcon from "../../assets/icons/chevron_right-24px.svg"
+import vectorIcon from "../../assets/icons/chevron_right-24px.svg";
 import "./WarehouseList.scss";
 
 export default function WarehouseList() {
@@ -98,50 +98,56 @@ export default function WarehouseList() {
   ]);
   return warehouses.map((warehouse) => {
     return (
-      <Link to={"/#"} key={warehouse.id} className="warehouse-link">
-        <div className="warehouse">
-          <div className="warehouse-wrapper">
-            <div className="location-container">
-              <div className="warehouse-container">
-                <h5 className="warehouse-container__label label">WAREHOUSE</h5>
-                <div className="warehouse-link__name">
-                <p className="warehouse-container__title">
-                  {warehouse.warehouse_name}
-                </p>
-                <img src={vectorIcon} alt="vector-icon"></img>
+      <>
+        <Link to={"/#"} key={warehouse.id} className="warehouse-link">
+          <div className="warehouse">
+            <div className="warehouse-wrapper">
+              <div className="location-container">
+                <div className="warehouse-container">
+                  <h5 className="warehouse-container__label label">
+                    WAREHOUSE
+                  </h5>
+                  <div className="warehouse-link__name">
+                    <p className="warehouse-container__title">
+                      {warehouse.warehouse_name}
+                    </p>
+                    <img src={vectorIcon} alt="vector-icon"></img>
+                  </div>
+                </div>
+                <div className="address-container">
+                  <h5 className="address-container__label label">ADDRESS</h5>
+                  <p className="address-container__title">
+                    {warehouse.address}, {warehouse.city}, {warehouse.country}
+                  </p>
                 </div>
               </div>
-              <div className="address-container">
-                <h5 className="address-container__label label">ADDRESS</h5>
-                <p className="address-container__title">{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
+              <div className="contact-container">
+                <div className="name-container">
+                  <h5 className="name-container__label label">CONTACT NAME</h5>
+                  <p className="name-container__title">
+                    {warehouse.contact_name}
+                  </p>
+                </div>
+                <div className="information-container">
+                  <h5 className="information-container__label label">
+                    CONTACT INFORMATION
+                  </h5>
+                  <p className="information-container__phone">
+                    {warehouse.contact_phone}
+                  </p>
+                  <p className="information-container__email">
+                    {warehouse.contact_email}
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="contact-container">
-              <div className="name-container">
-                <h5 className="name-container__label label">CONTACT NAME</h5>
-                <p className="name-container__title">
-                  {warehouse.contact_name}
-                </p>
-              </div>
-              <div className="information-container">
-                <h5 className="information-container__label label">
-                  CONTACT INFORMATION
-                </h5>
-                <p className="information-container__phone">
-                  {warehouse.contact_phone}
-                </p>
-                <p className="information-container__email">
-                  {warehouse.contact_email}
-                </p>
-              </div>
+            <div className="button-container">
+              <img className="action-icon" src={deleteIcon} alt="delete-icon"></img>
+              <img className="action-icon" src={editIcon} alt="edit-icon"></img>
             </div>
           </div>
-          <div className="button-container">
-            <img src={deleteIcon} alt="delete-icon"></img>
-            <img src={editIcon} alt="edit-icon"></img>
-          </div>
-        </div>
-      </Link>
+        </Link>
+      </>
     );
   });
 }
--- src/pages/Warehouse/Warehouse.js
+++ src/pages/Warehouse/Warehouse.js
@@ -1,25 +1,63 @@
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 import "./Warehouse.scss";
 import search from "../../assets/icons/search-24px.svg";
+import sort from "../../assets/icons/sort-24px.svg";
 
 function Warehouse() {
   return (
     <div className="warehouse-list-page">
-      <h1 className="heading">Warehouses</h1>
-      <div className="buttons">
-        <div className="search-bar">
-          <form>
-            <input
-              className="search-bar__input"
-              type="text"
-              placeholder="Search..."
-            ></input>
-          </form>
-          <img className="search-bar__icon" src={search} alt="search-Icon"></img>
+      <div className="warehouse-list-page__header">
+        <h1 className="heading">Warehouses</h1>
+        <div className="buttons">
+          <div className="search-bar">
+            <form>
+              <input
+                className="search-bar__input"
+                type="text"
+                placeholder="Search..."
+              ></input>
+            </form>
+            <img
+              className="search-bar__icon"
+              src={search}
+              alt="search-Icon"
+            ></img>
+          </div>
+          <div className="add-warehouse">
+            <p className="add-warehouse__text">+ Add New Warehouse</p>
+          </div>
         </div>
-        <div className="add-warehouse">
-          <p className="add-warehouse__text">+ Add New Warehouse</p>
+      </div>
+      <div className="fullscreen-labels">
+        <div className="warehouse-label label--fullscreen">
+          <p className="warehouse-label__text">WAREHOUSE</p>
+          <img
+            className="warehouse-label__icon"
+            src={sort}
+            alt="sort-icon"
+          ></img>
         </div>
+        <div className="address-label label--fullscreen">
+          <p className="address-label__text">ADDRESS</p>
+          <img className="address-label__icon" src={sort} alt="sort-icon"></img>
+        </div>
+        <div className="contact-name-label label--fullscreen">
+          <p className="contact-name-label__text">CONTACT NAME</p>
+          <img
+            className="contact-name-label__icon"
+            src={sort}
+            alt="sort-icon"
+          ></img>
+        </div>
+        <div className="contact-info-label label--fullscreen">
+          <p className="contact-info-label__text">CONTACT INFORMATION</p>
+          <img
+            className="contact-info-label__icon"
+            src={sort}
+            alt="sort-icon"
+          ></img>
+        </div>
+        <p className="actions-info-label label--fullscreen">ACTIONS</p>
       </div>
       <WarehouseList />
     </div>
