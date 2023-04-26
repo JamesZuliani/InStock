4ce97a6e0cb4c854082ee521c151f04ab6ca3b29
Nima Bargestan <nimam700@gmail.com>
2023-04-25 21:24:15 -0400

    finish header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,27 @@
-import LogoImg from "../../assests/Logo/InStock-Logo.svg";
+import LogoImg from "../../assets/logo/InStock-Logo.svg";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import "./Header.scss";
 export default function Header() {
+  const location = useLocation();
+
   return (
     <header className="header">
       <div className="header__container">
         <div className="header__logo-wrapper">
-          <img src={LogoImg} alt="inStock Logo" />
+          <Link to="/">
+            <img className="header__logo" src={LogoImg} alt="inStock Logo" />
+          </Link>
         </div>
         <nav className="nav">
-          <ul className="nav__nav-list">
-            <a className="nav__item" href="/">
-              Warehouses
-            </a>
-            <a className="nav__item" href="/">
-              Inventory
-            </a>
-          </ul>
+          <NavLink
+            to="/warehouse"
+            className={`nav__item ${location.pathname === "/" ? "active" : ""}`}
+          >
+            <h3 className="nav__text-link">Warehouses</h3>
+          </NavLink>
+          <NavLink to="/inventory" className="nav__item">
+            <h3 className="nav__text-link">Inventory</h3>
+          </NavLink>
         </nav>
       </div>
     </header>
