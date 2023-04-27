335514f2ed1283360ba5e72b97ded8c0eed92e05
James Zuliani <zulianijames@gmail.com>
2023-04-26 23:11:03 -0400

    warehouse list styling complete

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -17,9 +17,48 @@ export default function WarehouseList() {
     });
   }, []);
 
-  return warehouses.map((warehouse) => {
+  return warehouses.map((warehouse, index) => {
     return (
-      <Link to={"/#"} key={warehouse.id} className="warehouse-link">
+      <Link to={`/#`} key={warehouse.id} className="warehouse-link">
+        {/* this section of html is not rendered until tablet and desktop breakpoint */}
+        <div className={index!==warehouses.length-1 ? "warehouse--fullscreen warehouse--border" : "warehouse--fullscreen"}>
+          <div className="warehouse-link__name">
+            <p className="warehouse-container__title">
+              {warehouse.warehouse_name}
+            </p>
+            <img src={vectorIcon} alt="vector-icon" className="vector-icon"></img>
+          </div>
+          <div className="address-container">
+            <h5 className="address-container__label label">ADDRESS</h5>
+            <p className="address-container__title">
+              {warehouse.address}, {warehouse.city}, {warehouse.country}
+            </p>
+          </div>
+          <div className="name-container">
+            <h5 className="name-container__label label">CONTACT NAME</h5>
+            <p className="name-container__title">{warehouse.contact_name}</p>
+          </div>
+          <div className="information-container">
+            <h5 className="information-container__label label">
+              CONTACT INFORMATION
+            </h5>
+            <p className="information-container__phone">
+              {warehouse.contact_phone}
+            </p>
+            <p className="information-container__email">
+              {warehouse.contact_email}
+            </p>
+          </div>
+          <div className="button-container">
+            <img
+              className="action-icon"
+              src={deleteIcon}
+              alt="delete-icon"
+            ></img>
+            <img className="action-icon" src={editIcon} alt="edit-icon"></img>
+          </div>
+        </div>
+        {/* mobile view  */}
         <div className="warehouse">
           <div className="warehouse-wrapper">
             <div className="location-container">
--- src/pages/Warehouse/Warehouse.js
+++ src/pages/Warehouse/Warehouse.js
@@ -32,19 +32,19 @@ function Warehouse() {
         <div className="warehouse-label label--fullscreen">
           <p className="warehouse-label__text">WAREHOUSE</p>
           <img
-            className="warehouse-label__icon"
+            className="warehouse-label__icon sort-icon"
             src={sort}
             alt="sort-icon"
           ></img>
         </div>
         <div className="address-label label--fullscreen">
           <p className="address-label__text">ADDRESS</p>
-          <img className="address-label__icon" src={sort} alt="sort-icon"></img>
+          <img className="address-label__icon sort-icon" src={sort} alt="sort-icon"></img>
         </div>
         <div className="contact-name-label label--fullscreen">
           <p className="contact-name-label__text">CONTACT NAME</p>
           <img
-            className="contact-name-label__icon"
+            className="contact-name-label__icon sort-icon"
             src={sort}
             alt="sort-icon"
           ></img>
@@ -52,7 +52,7 @@ function Warehouse() {
         <div className="contact-info-label label--fullscreen">
           <p className="contact-info-label__text">CONTACT INFORMATION</p>
           <img
-            className="contact-info-label__icon"
+            className="contact-info-label__icon sort-icon"
             src={sort}
             alt="sort-icon"
           ></img>
