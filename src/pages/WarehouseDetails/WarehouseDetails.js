849da3597a7bf4e76fa44527b20e64a82b4757c7
Nima Bargestan <nimam700@gmail.com>
2023-04-29 01:54:03 -0400

    fix some styling in header

--- src/pages/WarehouseDetails/WarehouseDetails.js
+++ src/pages/WarehouseDetails/WarehouseDetails.js
@@ -1,24 +1,21 @@
-import "../pages/WarehouseDetails.scss";
-import BackButton from "../assets/icons/arrow_back-24px.svg";
+import "./WarehouseDetails.scss";
+import BackButton from "../../assets/icons/arrow_back-24px.svg";
 import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 function WarehouseDetails() {
   const { id } = useParams();
-  const [warehouseDetails, setwarehouseDetails] = useState([]);
+  const [warehouseDetails, setwarehouseDetails] = useState(null);
 
   useEffect(() => {
-    const fetchsingleWarehouse = async () => {
-      const { data } = await axios.get(
-        `http://localhost:8080/api/warehouses/${id}`
-      );
-      setwarehouseDetails(data);
-      console.log(data);
-    };
-    fetchsingleWarehouse();
-  });
-
+    axios.get(`http://localhost:8080/api/warehouses/${id}`).then((res) => {
+      setwarehouseDetails(res.data);
+    });
+  }, []);
+  if (!warehouseDetails) {
+    return;
+  }
   return (
     <div className="details">
       <div className="details__title--container">
@@ -31,7 +28,7 @@ function WarehouseDetails() {
             ></img>
           </Link>
           <div className="details__title">
-            <h1>{warehouseDetails.warehouse_name || "-"}</h1>
+            <h1>{warehouseDetails.warehouse_name}</h1>
           </div>
         </div>
         <Link to={`/warehouse/${id}/edit`} className="details__button--edit">
@@ -42,22 +39,38 @@ function WarehouseDetails() {
       <div className="details__section">
         <div className="details__address--container">
           <h3 className="details__subtitle">WAREHOUSE ADDRESS:</h3>
-          <p>
-            {warehouseDetails.address ? `${warehouseDetails.address}, ` : "-, "}
-            {warehouseDetails.city ? `${warehouseDetails.city}, ` : "-, "}
-            {warehouseDetails.country ? `${warehouseDetails.country}` : "-"}
+          <p className="details__subtitle-text">
+            {`${warehouseDetails.address}, `}
+            {`${warehouseDetails.city}, `}
+            {warehouseDetails.country}
           </p>
         </div>
         <div className="details__subtitle--container">
-          <div>
+          <div className="details__subtitle--sub-container">
             <h3 className="details__subtitle">CONTACT NAME:</h3>
-            <p>{warehouseDetails.contact_name ? `${warehouseDetails.contact_name}` : "- "}</p>
-            <p>{warehouseDetails.contact_position ? `${warehouseDetails.contact_position}` : "-"}</p>
+            <p>
+              {warehouseDetails.contact_name
+                ? `${warehouseDetails.contact_name}`
+                : "- "}
+            </p>
+            <p>
+              {warehouseDetails.contact_position
+                ? `${warehouseDetails.contact_position}`
+                : "-"}
+            </p>
           </div>
           <div className="details__contact--container">
             <h3 className="details__subtitle">CONTACT INFORMATION:</h3>
-            <p>{warehouseDetails.contact_phone ? `${warehouseDetails.contact_phone}` : "- "}</p>
-            <p>{warehouseDetails.contact_email ? `${warehouseDetails.contact_email}` : "-"}</p>
+            <p>
+              {warehouseDetails.contact_phone
+                ? `${warehouseDetails.contact_phone}`
+                : "- "}
+            </p>
+            <p>
+              {warehouseDetails.contact_email
+                ? `${warehouseDetails.contact_email}`
+                : "-"}
+            </p>
           </div>
         </div>
       </div>
